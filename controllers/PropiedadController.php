2f76de00463c0ee27f1bff6eb7b5a7e40c3b1bac
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-02-05 17:19:04 -0300

    Ya funciona la creación de una nueva propiedad y la validación de los campos

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,35 +1,44 @@
 <?php
 
 namespace Controllers;
+
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 use Intervention\Image\ImageManagerStatic as Image;
 
 
-class PropiedadController {
-    public static function index(Router $router) {
-        
+class PropiedadController
+{
+    public static function index(Router $router)
+    {
+
         // Traemos las propiedades de la DB.
         $propiedades = Propiedad::all();
 
+        // Muestra mensaje condicional.
+        $resultado = $_GET['resultado'] ?? null;
+
         $router->render('propiedades/admin', [
             "propiedades" => $propiedades, // Mostramos las propiedades obtenidas.
-            "mensaje" => false,
+            "resultado" => $resultado,
         ]);
     }
 
-    public static function crear(Router $router) {
+    public static function crear(Router $router)
+    {
 
-
-        // Instanciamos vacia propiedaD. 
+        // Instanciamos vacia propieda. 
         $propiedad = new Propiedad;
 
         // Obtenemos los vendedores.
         $vendedores = Vendedor::all();
 
+        // Arreglo con mensajes de errores.
+        $errores = Propiedad::getErrores();
+
         // Ejecutar el código después de que el usuario envie el formulario.
-        if($_SERVER['REQUEST_METHOD'] === 'POST') { 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             /** Crea una nueva instacia **/
             $propiedad = new Propiedad($_POST['propiedad']);
@@ -41,7 +50,7 @@ class PropiedadController {
 
             // Setear la imagen.
             // Realiza el size de la imagen con Intervention.
-            if($_FILES['propiedad']['tmp_name']['imagen']) { // Verificamos que haiga una imagen.
+            if ($_FILES['propiedad']['tmp_name']['imagen']) { // Verificamos que haiga una imagen.
                 $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
                 $propiedad->setImagen($nombreImg); // Asiganamos la iamgen si no hay o elimina la anterior y asigna la nueva.
             }
@@ -50,29 +59,31 @@ class PropiedadController {
             $errores = $propiedad->validar();
 
             // Revisar que el arreglo de errores este vacio.
-            if(empty($errores)) {
+            if (empty($errores)) {
 
                 // Crear la carpeta para subir imagenes.
-                if(!is_dir(CARPETA_IMAGENES)) {
+                if (!is_dir(CARPETA_IMAGENES)) {
                     mkdir(CARPETA_IMAGENES);
                 }
-                
+
                 // Guardar la imagen en el servidor.
                 $image->save(CARPETA_IMAGENES . $nombreImg);
 
                 // Guardamos en la base de datos.
                 $propiedad->guardar();
-            };            
+            };
         }
 
 
-            $router->render('propiedades/crear', [
-                'propiedad' => $propiedad,
-                'vendedores' => $vendedores,
-            ]);
+        $router->render('propiedades/crear', [
+            'propiedad' => $propiedad,
+            'vendedores' => $vendedores,
+            'errores' => $errores,
+        ]);
     }
 
-    public static function actualizar() {
+    public static function actualizar()
+    {
         echo 'Actualizando';
     }
 }

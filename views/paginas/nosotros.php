06e063e0171f9c8e0be8cc26b255750a29c04979
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-02-15 17:32:21 -0300

    Ya se puede ver el apartado 'nosotros'

---
+++ views/paginas/nosotros.php
@@ -0,0 +1,70 @@
+<main class="contenedor seccion">
+    <h1>Conoce Sobre Nosotros</h1>
+
+    <div class="contenedor-nosotros">
+
+        <div class="nosotros-imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img loading="lazy" src="build/img/nosotros.jpg" alt="Imagen Sobre Nostros">
+            </picture>
+        </div>
+
+        <div class="nosotros-texto">
+            <blockquote>
+                25 Años de Experiencia
+            </blockquote>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id soluta voluptatum
+                quos aspernatur repellendus veniam, dolorum saepe fugit, omnis beatae rerum.
+                Placeat laboriosam ratione facere. Perferendis culpa aspernatur id ullam.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id soluta voluptatum
+                quos aspernatur repellendus veniam, dolorum saepe fugit, omnis beatae rerum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id soluta voluptatum
+                quos aspernatur repellendus veniam, dolorum saepe fugit, omnis beatae rerum.
+                Placeat laboriosam ratione facere. Perferendis culpa aspernatur id ullam.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id soluta voluptatum
+                quos aspernatur repellendus veniam, dolorum saepe fugit, omnis beatae rerum.
+            </p>
+        </div>
+    </div>
+</main>
+
+<section class="contenedor contenedor-seccion">
+    <h1>Más Sobre Nosotros</h1>
+
+    <div class="iconos-nosotros">
+        <div class="icono">
+            <img src="build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
+            <h3>seguridad</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores velit laudantium cupiditate vitae id repellat
+                sint voluptatibus eveniet aliquid. Sed, velit. Vitae,
+                rerum? Modi velit, temporibus tempora consectetur
+                natus mollitia!</p>
+        </div>
+        <div class="icono">
+            <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+            <h3>Precio</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores velit laudantium cupiditate vitae id repellat
+                sint voluptatibus eveniet aliquid. Sed, velit. Vitae,
+                rerum? Modi velit, temporibus tempora consectetur
+                natus mollitia!</p>
+        </div>
+        <div class="icono">
+            <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+            <h3>A Tiempo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores velit laudantium cupiditate vitae id repellat
+                sint voluptatibus eveniet aliquid. Sed, velit. Vitae,
+                rerum? Modi velit, temporibus tempora consectetur
+                natus mollitia!</p>
+        </div>
+    </div>
+</section>
